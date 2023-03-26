3409e07d8e7464f3c8d83b07557e97901d807e43
Arina Kolodeznikova <arinak1017@gmail.com>
2023-03-26 14:23:49 -0400

    The MVC design pattern implemented.

---
+++ controllers/generalController.js
@@ -0,0 +1,125 @@
+const express = require("express");
+const router = express.Router();
+const rentalList = require("../models/rentals-db");
+
+// Setting up body-parser
+router.use(express.urlencoded({extended: false}))
+
+router.get("/", (req, res) => {
+    res.render("general/home", {
+        rentals: rentalList.getFeaturedRentals(),
+        title: "Home Page"
+    });
+})
+router.get("/sign-up", (req, res) => {
+    res.render("general/sign-up", {
+        title: "Sign-up Page"
+    });
+})
+router.post("/sign-up", (req, res) => {
+    console.log(req.body)
+    const { firstName, lastName, email, password } = req.body;
+    // Validating the input
+    let passedValidation = true;
+    let validationMessages = {};
+    // email regEX taken from: https://www.w3resource.com/javascript/form/email-validation.php
+    let emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let passwordRegEx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+}{"':;?/>.<,])(?=.*[a-zA-Z]).{8,12}$/;
+    if (typeof firstName !== "string" || firstName.trim().length === 0) {
+        passedValidation = false;
+        validationMessages.firstName = "Please enter your first name"; 
+    }
+    if (typeof lastName !== "string" || lastName.trim().length === 0) {
+        passedValidation = false;
+        validationMessages.lastName = "Please enter your last name"; 
+    }
+    if (typeof email !== "string" || email.trim().length === 0) {
+        passedValidation = false;
+        validationMessages.email = "Please enter your email"; 
+    }
+    else if (!emailRegEx.test(email)) {
+        passedValidation = false;
+        validationMessages.email = "You should enter a valid email address"; 
+    }
+    if (typeof password !== "string" || password.trim().length === 0) {
+        passedValidation = false;
+        validationMessages.password = "Please enter your password"; 
+    }
+    else if (!passwordRegEx.test(password)) {
+        passedValidation = false;
+        validationMessages.password = "Your password should be 8-12 characters long and contain at least one lowercase letter, uppercase letter, number and a symbol"; 
+    }
+    // If validation is passed, redirect to the Welcome Page / overwise reload
+    if(passedValidation) {
+        const sgMail = require("@sendgrid/mail");
+        sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+        const msg = {
+            to: email,
+            from: "arinak1017@gmail.com",
+            subject: "Registration confirmation",
+            html: `Hello, ${firstName} ${lastName}<br>
+                    <br>
+                    Welcome to RentAll Website!<br>
+                    <br>
+                    All the best,<br>
+                    Arina`
+        }
+        sgMail.send(msg)
+            .then(() => {
+                res.redirect(302, "/welcome");
+            })
+            .catch(err => {
+                console.log(err);
+                res.render("general/sign-up", {
+                    title: "Sign-up Page",
+                    validationMessages,
+                    values: req.body
+                });
+            });
+    }
+    else {
+        res.render("general/sign-up", {
+            title: "Sign-up Page",
+            validationMessages,
+            values: req.body
+        });
+    }
+})
+router.get("/welcome", (req, res) => {
+    res.render("general/welcome", {
+        title: "Welcome Page"
+    });
+})
+router.get("/log-in", (req, res) => {
+    res.render("general/log-in", {
+        title: "Log-in Page"
+    });
+})
+router.post("/log-in", (req, res) => {
+    console.log(req.body)
+    const { email, password } = req.body;
+    // Validating the input
+    let passedValidation = true;
+    let validationMessages = {};
+    if (typeof email !== "string" || email.trim().length === 0) {
+        passedValidation = false;
+        validationMessages.email = "Please enter your email"; 
+    }
+    if (typeof password !== "string" || password.trim().length === 0) {
+        passedValidation = false;
+        validationMessages.password = "Please enter your password"; 
+    }
+    // Reloading if validation is not passed
+    if(passedValidation) {
+        res.send("Success!");
+    }
+    else {
+        res.render("general/log-in", {
+            title: "Log-in Page",
+            validationMessages,
+            values: req.body
+        });
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -12,7 +12,6 @@
 const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
-const rentalList = require("./models/rentals-db");
 
 // Setting up dotenv
 const dotenv = require("dotenv");
@@ -27,136 +26,15 @@ app.engine(".hbs", exphbs.engine({
 }))
 app.set("view engine", ".hbs");
 
-// Setting up body-parser
-app.use(express.urlencoded({extended: false}))
-
 // Making the "assets: folder public
 app.use(express.static(path.join(__dirname, "/assets")));
 
-// Add your routes here
-// e.g. app.get() { ... }
-app.get("/", (req, res) => {
-    res.render("home", {
-        rentals: rentalList.getFeaturedRentals(),
-        title: "Home Page"
-    });
-})
-app.get("/rentals", (req, res) => {
-    res.render("rentals", {
-        rentalsGrouped: rentalList.getRentalsByCityAndProvince(),
-        title: "Rentals Page"
-    });
-})
-app.get("/sign-up", (req, res) => {
-    res.render("sign-up", {
-        title: "Sign-up Page"
-    });
-})
-app.post("/sign-up", (req, res) => {
-    console.log(req.body)
-    const { firstName, lastName, email, password } = req.body;
-    // Validating the input
-    let passedValidation = true;
-    let validationMessages = {};
-    // email regEX taken from: https://www.w3resource.com/javascript/form/email-validation.php
-    let emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let passwordRegEx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+}{"':;?/>.<,])(?=.*[a-zA-Z]).{8,12}$/;
-    if (typeof firstName !== "string" || firstName.trim().length === 0) {
-        passedValidation = false;
-        validationMessages.firstName = "Please enter your first name"; 
-    }
-    if (typeof lastName !== "string" || lastName.trim().length === 0) {
-        passedValidation = false;
-        validationMessages.lastName = "Please enter your last name"; 
-    }
-    if (typeof email !== "string" || email.trim().length === 0) {
-        passedValidation = false;
-        validationMessages.email = "Please enter your email"; 
-    }
-    else if (!emailRegEx.test(email)) {
-        passedValidation = false;
-        validationMessages.email = "You should enter a valid email address"; 
-    }
-    if (typeof password !== "string" || password.trim().length === 0) {
-        passedValidation = false;
-        validationMessages.password = "Please enter your password"; 
-    }
-    else if (!passwordRegEx.test(password)) {
-        passedValidation = false;
-        validationMessages.password = "Your password should be 8-12 characters long and contain at least one lowercase letter, uppercase letter, number and a symbol"; 
-    }
-    // If validation is passed, redirect to the Welcome Page / overwise reload
-    if(passedValidation) {
-        const sgMail = require("@sendgrid/mail");
-        sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
-        const msg = {
-            to: email,
-            from: "arinak1017@gmail.com",
-            subject: "Registration confirmation",
-            html: `Hello, ${firstName} ${lastName}<br>
-                    <br>
-                    Welcome to RentAll Website!<br>
-                    <br>
-                    All the best,<br>
-                    Arina`
-        }
-        sgMail.send(msg)
-            .then(() => {
-                res.redirect(302, "/welcome");
-            })
-            .catch(err => {
-                console.log(err);
-                res.render("sign-up", {
-                    title: "Sign-up Page",
-                    validationMessages,
-                    values: req.body
-                });
-            });
-    }
-    else {
-        res.render("sign-up", {
-            title: "Sign-up Page",
-            validationMessages,
-            values: req.body
-        });
-    }
-})
-app.get("/welcome", (req, res) => {
-    res.render("welcome", {
-        title: "Welcome Page"
-    });
-})
-app.get("/log-in", (req, res) => {
-    res.render("log-in", {
-        title: "Log-in Page"
-    });
-})
-app.post("/log-in", (req, res) => {
-    console.log(req.body)
-    const { email, password } = req.body;
-    // Validating the input
-    let passedValidation = true;
-    let validationMessages = {};
-    if (typeof email !== "string" || email.trim().length === 0) {
-        passedValidation = false;
-        validationMessages.email = "Please enter your email"; 
-    }
-    if (typeof password !== "string" || password.trim().length === 0) {
-        passedValidation = false;
-        validationMessages.password = "Please enter your password"; 
-    }
-    // Reloading if validation is not passed
-    if(passedValidation) {
-        res.send("Success!");
-    }
-    else {
-        res.render("log-in", {
-            title: "Log-in Page",
-            validationMessages,
-            values: req.body
-        });
-    }
-})
+// Loading the controllers into express.
+const generalController = require("./controllers/generalController");
+const rentalsController = require("./controllers/rentalsController");
+
+app.use("/", generalController);
+app.use("/rentals", rentalsController);
 
 // *** DO NOT MODIFY THE LINES BELOW ***
 
