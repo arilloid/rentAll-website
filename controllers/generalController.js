af15b9c5f60a7b4c2b90c6fe0d7363074429a5e6
Arina Kolodeznikova <arinak1017@gmail.com>
2023-03-27 15:07:44 -0400

    Email uniqueness check added.

--- controllers/generalController.js
+++ controllers/generalController.js
@@ -47,55 +47,67 @@ router.post("/sign-up", (req, res) => {
         passedValidation = false;
         validationMessages.password = "Your password should be 8-12 characters long and contain at least one lowercase letter, uppercase letter, number and a symbol"; 
     }
-    // If validation is passed, redirect to the Welcome Page / overwise reload
-    if(passedValidation) {
-        const newUser = new userModel({firstName, lastName, email, password});
+    // Checking the uniqueness of the entered email
+    userModel.find({ email: email})
+        .then((users) => {
+            if (users.length > 0) {
+                // email already exists in the database
+                passedValidation = false;
+                validationMessages.email = "This email is already associated with the existing account. Please enter another one."; 
+            }
+            // If validation is passed, save user to the database
+            if(passedValidation) {
+                const newUser = new userModel({firstName, lastName, email, password});
 
-        newUser.save()
-            .then(userSaved => {
-                console.log(`User ${userSaved.firstName} has been added to the database.`);
-                const sgMail = require("@sendgrid/mail");
-                sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
-                const msg = {
-                    to: email,
-                    from: "arinak1017@gmail.com",
-                    subject: "Registration confirmation",
-                    html: `Hello, ${firstName} ${lastName}<br>
-                            <br>
-                            Welcome to RentAll Website!<br>
-                            <br>
-                            All the best,<br>
-                            Arina`
-                }
-                sgMail.send(msg)
-                    .then(() => {
-                        res.redirect(302, "/welcome");
+                newUser.save()
+                    .then(userSaved => {
+                        console.log(`User ${userSaved.firstName} has been added to the database.`);
+                        const sgMail = require("@sendgrid/mail");
+                        sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+                        const msg = {
+                            to: email,
+                            from: "arinak1017@gmail.com",
+                            subject: "Registration confirmation",
+                            html: `Hello, ${firstName} ${lastName}<br>
+                                    <br>
+                                    Welcome to RentAll Website!<br>
+                                    <br>
+                                    All the best,<br>
+                                    Arina`
+                        }
+                        sgMail.send(msg)
+                            .then(() => {
+                                res.redirect(302, "/welcome");
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.render("general/sign-up", {
+                                    title: "Sign-up Page",
+                                    validationMessages,
+                                    values: req.body
+                                });
+                            });
                     })
                     .catch(err => {
-                        console.log(err);
+                        console.log(`Error adding user to the database ... ${err}`);
                         res.render("general/sign-up", {
                             title: "Sign-up Page",
                             validationMessages,
                             values: req.body
                         });
                     });
-            })
-            .catch(err => {
-                console.log(`Error adding user to the database ... ${err}`);
+            }
+            else {
                 res.render("general/sign-up", {
                     title: "Sign-up Page",
                     validationMessages,
                     values: req.body
                 });
-            });
-    }
-    else {
-        res.render("general/sign-up", {
-            title: "Sign-up Page",
-            validationMessages,
-            values: req.body
+            }
+        })
+        .catch((err) => {
+            console.log(`Error occurred while checking email existence ... ${err}`);
         });
-    }
 })
 router.get("/welcome", (req, res) => {
     res.render("general/welcome", {
